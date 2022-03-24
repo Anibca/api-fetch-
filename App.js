2e597a85046c833458274c1c3f48ec1ae195d95b
Anibca <38027956+Anibca@users.noreply.github.com>
2022-03-24 18:02:37 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import './App.css';
+class App extends React.Component {
+
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+"https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> {
+				items.map((item) => (
+				<ol key = { item.id } >
+        usd:{item.usd}
+					</ol>
+				))
+			}
+		</div>
+	);
+}
+}
+export default App;
